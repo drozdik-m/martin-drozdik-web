f2fac3cd7658f00497a8a9189575448aea0816ed
Martin Drozdík <drozdik.m@seznam.cz>
2021-09-01 13:40:35 +0200

    Add desktop menu

--- src/MartinDrozdik.Web/wwwroot/Web/Menu/Menu.ts
+++ src/MartinDrozdik.Web/wwwroot/Web/Menu/Menu.ts
@@ -1,7 +1,78 @@
-﻿/*import { WindowEvents } from "@drozdik.m/window-events";
-
+﻿import { Menu } from "@drozdik.m/menu";
+import { WindowEvents } from "@drozdik.m/window-events";
 
 WindowEvents.OnDOMReady.Add(function ()
 {
+    let menuElement = document.getElementById("menu")
+    if (!menuElement)
+    {
+        console.error("Element #menu not found");
+        return;
+    }
 
-});*/
+    let menuToggleElement = document.getElementById("menuToggle");
+    if (!menuToggleElement)
+    {
+        console.error("Element #menuToggle not found");
+        return;
+    }
+
+    let menu = new Menu(menuElement)
+        .AddToggleButton(menuToggleElement)
+        .SetWidthBreaking(1000);
+
+    menu.OnMenuOpen.Add(function ()
+    {
+        function HeightWithMargin(el: HTMLElement)
+        {
+            var height = el.offsetHeight;
+            var style = getComputedStyle(el);
+
+            height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+            return height;
+        }
+
+        let ulElement = document.getElementById("menu").querySelector("ul")
+        let liElements = ulElement.querySelectorAll("li")
+        var liHeight = HeightWithMargin(liElements.item(0))
+        var liCount = liElements.length;
+        ulElement.style.setProperty("max-height", liCount * liHeight + "px")
+
+    });
+
+    menu.OnMenuClose.Add(function ()
+    {
+        let ulElement = document.getElementById("menu").querySelector("ul")
+        ulElement.style.setProperty("max-height", "0")
+    });
+
+    /*menu.OnDesktopMenu.Add(function ()
+    {
+        console.log("Desktop menu")
+    })
+
+    menu.OnMobileMenu.Add(function ()
+    {
+        console.log("Mobile menu")
+    })
+
+    menu.OnDesktopMenuOnce.Add(function ()
+    {
+        console.log("Desktop menu once")
+    })
+
+    menu.OnMobileMenuOnce.Add(function ()
+    {
+        console.log("Mobile menu once")
+    })
+
+    menu.OnMenuOpen.Add(function ()
+    {
+        console.log("Menu open")
+    })
+
+    menu.OnMenuClose.Add(function ()
+    {
+        console.log("Menu close")
+    })*/
+});
