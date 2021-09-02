f4ea96e6c7af08c976fa51cbc2951b6b995ec43d
Martin Drozdík <drozdik.m@seznam.cz>
2021-09-02 12:43:35 +0200

    Add basic desktop hero header

--- src/MartinDrozdik.Web/wwwroot/Web/HeroHeader/HeroHeader.ts
+++ src/MartinDrozdik.Web/wwwroot/Web/HeroHeader/HeroHeader.ts
@@ -1,7 +1,8 @@
-﻿/*import { WindowEvents } from "@drozdik.m/window-events";
+﻿import { WindowEvents } from "@drozdik.m/window-events";
 
 
 WindowEvents.OnDOMReady.Add(function ()
 {
-
-});*/
+    var opacities = [0.0175, 0.05, 0.1, 0.25]
+    var contentElement = document.getElementById("heroHeaderContent");
+});
