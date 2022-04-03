c8245ebae77ff7d3b646f4978da2506718e5d586
Martin Drozdík <drozdik.m@seznam.cz>
2022-04-03 20:04:57 +0200

    Install MudBlazor, prepare for user API

---
+++ src/MartinDrozdik.Web.Admin/Server/Controllers/UserApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MartinDrozdik.Data.Models.Authentication;
+using MartinDrozdik.Data.Models.UserIdentity;
+using MartinDrozdik.Web.Admin.Server.Models.User;
+using MartinDrozdik.Web.Facades.User;
+using MartinDrozdik.Web.Facades.User.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MartinDrozdik.Web.Admin.Server.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    [Authorize(Roles = UserRoles.Developer + "," + UserRoles.Admin)]
+    public class UserApiController : Controller
+    {
+        //TODO
+    }
+}
