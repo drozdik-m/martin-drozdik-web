7d635431cec8c935716c9ef8b982f42dd67adf6d
Martin Drozdík <drozdik.m@seznam.cz>
2022-04-27 17:46:49 +0200

    Start modeling models for rich text editor

---
+++ src/MartinDrozdik.Data.Models/Text/Blocks/ImportantTextBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinDrozdik.Data.Models.Text.Blocks
+{
+    public class ImportantTextBlock : RichTextBlock
+    {
+        /// <inheritdoc/>
+        public override string TypeName => "Important text";
+
+
+        /// <summary>
+        /// The important text to show
+        /// </summary>
+        public string Text { get; set; } = string.Empty;
+
+        /// <inheritdoc/>
+        public override string ToHTML()
+        {
+            return $"<p class=\"impotantText\">{Sanitized(Text)}</p>";
+        }
+    }
+}
---
+++ src/MartinDrozdik.Data.Models/Text/Blocks/Paragraph.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinDrozdik.Data.Models.Text.Blocks
+{
+    public class Paragraph
+    {
+    }
+}
