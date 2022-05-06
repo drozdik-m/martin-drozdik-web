558737e736c2b8fb46737f4bfa5e12862df56793
Martin Drozdík <drozdik.m@seznam.cz>
2022-05-06 15:12:29 +0200

    Upgrade some design details, start working on database-fetched CV rendering

--- src/MartinDrozdik.Web/Views/Home/_Biography.cshtml.cs
+++ src/MartinDrozdik.Web/Views/Home/_Biography.cshtml.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MartinDrozdik.Data.Models.CV;
 
 namespace MartinDrozdik.Web.Views.Home
 {
     public class BiographyModel
     {
-        public static BiographyModel Default => new BiographyModel();
+        public IEnumerable<WorkExperience> WorkExperiences { get; }
+        public IEnumerable<Education> Educations { get; }
+        public IEnumerable<LanguageSkill> LanguageSkills { get; }
+
+        public BiographyModel(IEnumerable<WorkExperience> workExperiences,
+            IEnumerable<Education> educations,
+            IEnumerable<LanguageSkill> languageSkills)
+        {
+            WorkExperiences = workExperiences;
+            Educations = educations;
+            LanguageSkills = languageSkills;
+        }
     }
 }
