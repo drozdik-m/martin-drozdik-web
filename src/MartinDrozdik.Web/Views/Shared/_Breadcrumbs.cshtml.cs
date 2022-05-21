ae6f8d28b71e8e13d087041cff44a97b9879aeee
Martin Drozdík <drozdik.m@seznam.cz>
2022-05-21 20:21:00 +0200

    Add breadcrumbs

---
+++ src/MartinDrozdik.Web/Views/Shared/_Breadcrumbs.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MartinDrozdik.Data.Models.Projects;
+using MartinDrozdik.Web.Views.Home;
+
+namespace MartinDrozdik.Web.Views.Shared
+{
+    public class BreadcrumbsModel
+    {
+        public BreadcrumbsModel(IEnumerable<BreadcrumbModel> breadcrumbs)
+        {
+            Breadcrumbs = breadcrumbs;
+        }
+
+        public IEnumerable<BreadcrumbModel> Breadcrumbs { get; }
+
+    }
+
+    public class BreadcrumbModel
+    {
+        public BreadcrumbModel(string text, string uri, BreadcrumbType type, BreadcrumbScheme scheme)
+        {
+            Text = text;
+            Uri = uri;
+            Type = type;
+            Scheme = scheme;
+        }
+
+        public string Text { get; }
+        public string Uri { get; }
+        public BreadcrumbType Type { get; }
+        public BreadcrumbScheme Scheme { get; }
+
+        public static string SchemeToClass(BreadcrumbScheme scheme)
+        {
+            return scheme switch
+            {
+                BreadcrumbScheme.Black => "black",
+                BreadcrumbScheme.Dark => "dark",
+                BreadcrumbScheme.Red => "red",
+                BreadcrumbScheme.DarkRed => "darkRed",
+                BreadcrumbScheme.DarkerRed => "darkerRed",
+                BreadcrumbScheme.White => "white",
+                _ => throw new Exception("Unknown scheme"),
+            };
+        }
+
+        public static string TypeToClass(BreadcrumbType scheme)
+        {
+            return scheme switch
+            {
+                BreadcrumbType.Home => "home",
+                BreadcrumbType.Projects => "projects",
+                BreadcrumbType.Articles => "articles",
+                BreadcrumbType.Article => "article",
+                _ => throw new Exception("Unknown type"),
+            };
+        }
+    }
+
+    public enum BreadcrumbScheme
+    {
+        Black,
+        Dark,
+        Red,
+        DarkRed,
+        DarkerRed,
+        White,
+    }
+    public enum BreadcrumbType
+    {
+        Home,
+        Projects,
+        Articles,
+        Article
+    }
+}
